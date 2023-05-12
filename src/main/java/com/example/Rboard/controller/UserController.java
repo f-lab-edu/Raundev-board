fa4bee5a24d11bb88b5a700b85d30712f74c7414
Raun <lbil7641@gmail.com>
2023-05-12 18:00:55 +0900

    주석 추가

--- src/main/java/com/example/Rboard/controller/UserController.java
+++ src/main/java/com/example/Rboard/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.example.Rboard.controller;
 
+import com.example.Rboard.domain.User;
 import com.example.Rboard.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -14,8 +18,15 @@ public class UserController {
     private final UserService userService;  //userService
     private Logger logger = LoggerFactory.getLogger(UserService.class); //looger
 
+    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
+
+    @GetMapping("/{userId}")
+    public User findUser(@PathVariable String userId) {
+        User user = userService.findUser(userId);
+        return user;
+    }
 }
--- src/main/java/com/example/Rboard/repository/MybatisArticleRepository.java
+++ src/main/java/com/example/Rboard/repository/MybatisArticleRepository.java
@@ -5,7 +5,7 @@ import com.example.Rboard.domain.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Component;
 
-@Mapper
+@Mapper //XML파일 도는 어노테이션에 기재된 SQL 쿼리 명령을 수행할 수 있게 하는 어노테이션, 해당 인터페이스를 MyBais mapper 빈으로 등록해줌
 @Component
 public interface MybatisArticleRepository extends ArticleRepository{
 
